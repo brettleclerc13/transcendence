d62d6299cb8224511e32cc5fc2d39d5971fb7618
brettleclerc13 <brettleclerc13@gmail.com>
2024-10-25 11:53:13 +0200

    Switch to a gradient teal-to-transparent effect on homepage
    The gradient layer renders a smoother transition from grid boxes to plain bg,
    instead of playing with the opacity. It also demands less load to carry out.

--- Frontend/transcendence_front/src/app/page.tsx
+++ Frontend/transcendence_front/src/app/page.tsx
@@ -28,14 +28,21 @@ export default function Home() {
 
   return (
     <div className="relative h-screen flex justify-center items-center bg-teal-600">
+
+		{/* Layer to cover bottom half of grid elements with a smooth gradient teal to transparent effect */}
+		<div className="absolute inset-x-0 bottom-0 h-screen bg-gradient-to-t from-teal-600 from-30% pointer-events-none z-10"></div>
+
+		{/* Similar gradient effect on the top 10% for a finishing touch*/}
+		<div className="absolute inset-x-0 top-0 h-screen bg-gradient-to-b from-teal-600 opacity-70 to-10% pointer-events-none z-10"></div>
+
         <Grid ref={gridRef} onDisappear={handleDisappear}/>
         {showFirstLayer && (
-          <div className="absolute shadow-lg shadow-gray-900 bg-gray-800 text-white rounded-full px-7 py-5 pointer-events-none transition-opacity duration-500 ${!showFirstLayer ? 'opacity-0' : 'opacity-100'}">
+          <div className="absolute shadow-lg shadow-gray-900 bg-gray-800 text-white rounded-full px-7 py-5 pointer-events-none transition-opacity duration-500 ${!showFirstLayer ? 'opacity-0' : 'opacity-100'} z-20">
             <Title />
           </div>
         )}
         {showBlackLayer && (
-				<div className="absolute inset-0 bg-black transition-opacity duration-500 opacity-100 z-20">
+				<div className="absolute inset-0 bg-black transition-opacity duration-500 opacity-100 z-30">
 					<BackButton onReappear={handleReappear}/>
 				</div>
 			)}
