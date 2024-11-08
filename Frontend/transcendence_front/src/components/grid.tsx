41d21a848a8d8bf69b88908c7c8a25a719c3c95b
EmlHT <emilien.houot@gmail.com>
2024-11-08 19:37:35 +0100

    Build front is done

--- Frontend/transcendence_front/src/components/grid.tsx
+++ Frontend/transcendence_front/src/components/grid.tsx
@@ -1,11 +1,19 @@
 "use client"
 
-import { forwardRef, useEffect, useImperativeHandle, useRef } from 'react';
+import { HoverableDiv } from '@/app/types';
+import { forwardRef, useCallback, useEffect, useImperativeHandle, useRef } from 'react';
 
 const Grid = forwardRef(function Grid({ onDisappear }: { onDisappear: () => void}, ref) {
 	const containerRef = useRef<HTMLDivElement>(null);
 	const cellSize = 50;
-	const titleHeight = 200;
+	// const titleHeight = 200;
+
+	const handleClick = useCallback((cell: HoverableDiv) => {
+			if (cell.hoverCount && cell.hoverCount >= 2) {
+				cell.style.pointerEvents = 'none'; // Disable further interactions with the cell
+				onDisappear();
+			}
+		}, [onDisappear]);
 
 	useImperativeHandle(ref, () => ({
 		resetGrid() {
@@ -16,11 +24,37 @@ const Grid = forwardRef(function Grid({ onDisappear }: { onDisappear: () => void
 		}
 	}));
 
+	const generateGrid = useCallback((largeurPage: number) => {
+		const container = containerRef.current;
+		if (container) container.innerHTML = '';
+
+		const numCols = Math.floor(largeurPage / cellSize); // Nombre de colonnes basées sur la largeur disponible
+		const numRows = Math.floor(window.innerHeight / cellSize);
+		// const middleScreenY = window.innerHeight / 2;
+
+		for (let i = 0; i < numRows; i++) {
+			for (let j = 0; j < numCols; j++) {
+				const cell = document.createElement("div") as HoverableDiv;
+				cell.classList.add("cell");
+				cell.style.width = `${cellSize}px`;
+				cell.style.height = `${cellSize}px`;
+
+				// Calcul de la distance de chaque cellule par rapport au centre de la page
+				// const cellYPosition = i * cellSize;
+				// const fadeStart = middleScreenY - titleHeight / 2; // Le début du fondu (haut du titre)
+				// const fadeEnd = middleScreenY + titleHeight / 2; // La fin du fondu (bas du titre)
+
+				cell.addEventListener("mouseenter", () => isHovered(cell), true);
+				cell.addEventListener('click', () => handleClick(cell), true);
+
+				cell.className = 'border border-teal-900/50 flex items-center justify-center w-[50px] h-[50px] overflow-hidden';
+				container!.appendChild(cell);
+			}
+		}
+	}, [cellSize, handleClick]);
+
 	useEffect(() => {
-		const updateGrid = () => {
-			const largeurPage = window.innerWidth;
-			generateGrid(largeurPage);
-		  };
+		const updateGrid = () => generateGrid(window.innerWidth);
 
 		// Appel initial pour générer la grille au chargement
 		updateGrid();
@@ -30,9 +64,9 @@ const Grid = forwardRef(function Grid({ onDisappear }: { onDisappear: () => void
 
 		// Cleanup de l'événement lors du démontage du composant
 		return () => window.removeEventListener('resize', updateGrid);
-	}, []);
+	}, [generateGrid]);
 
-	function isHovered(cell: HTMLDivElement) {
+	function isHovered(cell: HoverableDiv) {
 		if (!cell.hoverCount) {
 			cell.initColor = {
 				r : 13,
@@ -49,57 +83,20 @@ const Grid = forwardRef(function Grid({ onDisappear }: { onDisappear: () => void
 		}
 		const darkeningPercentage = Math.max(1 - (0.5 * cell.hoverCount), 0);
 
-		const r = Math.floor(cell.initColor.r * darkeningPercentage);
-		const g = Math.floor(cell.initColor.g * darkeningPercentage);
-		const b = Math.floor(cell.initColor.b * darkeningPercentage);
+		const r = Math.floor(cell.initColor!.r * darkeningPercentage);
+		const g = Math.floor(cell.initColor!.g * darkeningPercentage);
+		const b = Math.floor(cell.initColor!.b * darkeningPercentage);
 
 		cell.style.backgroundColor = `rgba(${r}, ${g}, ${b}, 1)`;
 	}
 
-	function handleClick(cell: HTMLDivElement) {
-		if (cell.hoverCount >= 2) {
-			cell.style.pointerEvents = 'none'; // Disable further interactions with the cell
-			onDisappear();
-		}
-	}
-
-	function generateGrid(largeurPage: number) {
-		const container = containerRef.current;
-		if (container) container.innerHTML = '';
-
-		const numCols = Math.floor(largeurPage / cellSize); // Nombre de colonnes basées sur la largeur disponible
-		const numRows = Math.floor(window.innerHeight / cellSize);
-		const middleScreenY = window.innerHeight / 2;
-
-		for (let i = 0; i < numRows; i++) {
-			for (let j = 0; j < numCols; j++) {
-				const cell = document.createElement("div");
-				cell.classList.add("cell");
-				cell.style.width = `${cellSize}px`;
-				cell.style.height = `${cellSize}px`;
-
-				// Calcul de la distance de chaque cellule par rapport au centre de la page
-				const cellYPosition = i * cellSize;
-				const fadeStart = middleScreenY - titleHeight / 2; // Le début du fondu (haut du titre)
-				const fadeEnd = middleScreenY + titleHeight / 2; // La fin du fondu (bas du titre)
-
-				cell.addEventListener("mouseenter", () => isHovered(cell), true);
-				cell.addEventListener('click', () => handleClick(cell), true);
-
-				cell.className = 'border border-teal-900/50 flex items-center justify-center w-[50px] h-[50px] overflow-hidden';
-
-				container.appendChild(cell);
-			}
-		}
-	}
-
-	const resetGrid = () => {
-		if (containerRef.current) {
-			containerRef.current.innerHTML = ''; // Clear previous grid cells
-			const largeurPage = window.innerWidth; // Recalculate width for grid
-			generateGrid(largeurPage); // Generate the grid again
-		}
-	};
+	// const resetGrid = () => {
+	// 	if (containerRef.current) {
+	// 		containerRef.current.innerHTML = ''; // Clear previous grid cells
+	// 		const largeurPage = window.innerWidth; // Recalculate width for grid
+	// 		generateGrid(largeurPage); // Generate the grid again
+	// 	}
+	// };
 
 	return <div ref={containerRef} id="container" className="flex flex-wrap w-full overflow-hidden"></div>;
 });
