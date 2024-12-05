fdc6aa1d341a24348f3285d5b71396d15ea2dfe4
EmlHT <emilien.houot@gmail.com>
2024-12-05 16:00:42 +0100

    Added aspect for the dropdown menu

--- Frontend/src/app/Header/headerComponent.tsx
+++ Frontend/src/app/Header/headerComponent.tsx
@@ -87,8 +87,16 @@ export default function HeaderComponent({goToSection, isLoggedIn, userProfile}:M
 
 						{isDropdownOpen ? (
 							<div className="dropdown-menu" ref={dropdownRef}>
-								<button className="dropdown-item" onClick={() => goToSection("profile")}>Profile</button>
-								<button className="dropdown-item logout" onClick={() => console.log("Logging Out...")}>Log Out</button>
+								<button className="dropdown-item" onClick={() => goToSection("profile")}>
+									<svg className="icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+										<path fill-rule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clip-rule="evenodd"/>
+									</svg> Profile
+								</button>
+								<button className="dropdown-item logout" onClick={() => console.log("Logging Out...")}>
+									<svg className="icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+										<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H8m12 0-4 4m4-4-4-4M9 4H7a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h2"/>
+									</svg> Log Out
+								</button>
 							</div>
 						) : null}
 					</div>
