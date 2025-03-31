2baefb7c733f687fafc5bba447af8c3b5e8e57d5
EmlHT <emilien.houot@gmail.com>
2025-03-31 14:50:17 +0200

    hero tailwind removed

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,12 +1,13 @@
 import Hero from "./Hero/hero";
 import HeaderComponent from "./Header/headerComponent";
 import "./page.css";
+import HeroWrapper from "./Hero/heroWrapper";
 
 export default async function Home() {
 	return (
 		<div className="app-container">
 			<HeaderComponent />
-			<Hero />
+			<HeroWrapper />
 		</div>
 	);
 }
